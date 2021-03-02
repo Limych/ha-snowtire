1d96a00927ffea7fba9aa58c6675cd4a62151bfc
Andrey Khrolenok <andrey@khrolenok.ru>
2021-03-03 01:14:54 +0300

    Update requirements

--- custom_components/snowtire/const.py
+++ custom_components/snowtire/const.py
@@ -8,7 +8,7 @@ https://github.com/Limych/ha-snowtire/
 # Base component constants
 NAME = "Snowtire Sensor"
 DOMAIN = "snowtire"
-VERSION = "0.1.0b2"
+VERSION = "0.1.0b2.dev0"
 ISSUE_URL = "https://github.com/Limych/ha-snowtire/issues"
 
 STARTUP_MESSAGE = f"""
