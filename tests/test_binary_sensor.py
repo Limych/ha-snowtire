b8b048a0e2ccea11df03f67bef8e86c1bf764179
Andrey Khrolenok <andrey@khrolenok.ru>
2021-05-09 12:24:12 +0300

    Add German translation #21

--- tests/test_binary_sensor.py
+++ tests/test_binary_sensor.py
@@ -27,14 +27,14 @@ from custom_components.snowtire.const import (
     ICON_WINTER,
 )
 
-TEST_UNIQUE_ID = "test_id"
-TEST_NAME = "test_name"
-TEST_WEATHER_ENTITY = "weather.test"
-TEST_DAYS = 1
+MOCK_UNIQUE_ID = "test_id"
+MOCK_NAME = "test_name"
+MOCK_WEATHER_ENTITY = "weather.test"
+MOCK_DAYS = 1
 
-TEST_CONFIG = {
+MOCK_CONFIG = {
     CONF_PLATFORM: DOMAIN,
-    CONF_WEATHER: TEST_WEATHER_ENTITY,
+    CONF_WEATHER: MOCK_WEATHER_ENTITY,
 }
 
 
@@ -42,7 +42,7 @@ TEST_CONFIG = {
 def default_sensor(hass: HomeAssistant):
     """Create an AverageSensor with default values."""
     entity = SnowtireBinarySensor(
-        TEST_UNIQUE_ID, TEST_NAME, TEST_WEATHER_ENTITY, TEST_DAYS
+        MOCK_UNIQUE_ID, MOCK_NAME, MOCK_WEATHER_ENTITY, MOCK_DAYS
     )
     entity.hass = hass
     return entity
@@ -52,27 +52,27 @@ async def test_setup_platform(hass: HomeAssistant):
     """Test platform setup."""
     async_add_entities = MagicMock()
 
-    await async_setup_platform(hass, TEST_CONFIG, async_add_entities, None)
+    await async_setup_platform(hass, MOCK_CONFIG, async_add_entities, None)
     assert async_add_entities.called
 
 
 async def test_sensor_initialization(default_sensor):
     """Test sensor initialization."""
-    assert default_sensor.unique_id == TEST_UNIQUE_ID
+    assert default_sensor.unique_id == MOCK_UNIQUE_ID
     assert default_sensor.device_class == f"{DOMAIN}__type"
-    assert default_sensor.name == TEST_NAME
+    assert default_sensor.name == MOCK_NAME
     assert default_sensor.should_poll is False
     assert default_sensor.is_on is None
     assert default_sensor.icon == ICON_WINTER
     assert default_sensor.available is False
 
-    entity = SnowtireBinarySensor(None, TEST_NAME, TEST_WEATHER_ENTITY, TEST_DAYS)
+    entity = SnowtireBinarySensor(None, MOCK_NAME, MOCK_WEATHER_ENTITY, MOCK_DAYS)
     assert entity.unique_id is None
 
     entity = SnowtireBinarySensor(
-        "__legacy__", TEST_NAME, TEST_WEATHER_ENTITY, TEST_DAYS
+        "__legacy__", MOCK_NAME, MOCK_WEATHER_ENTITY, MOCK_DAYS
     )
-    assert entity.unique_id == f"{TEST_WEATHER_ENTITY}-1"
+    assert entity.unique_id == f"{MOCK_WEATHER_ENTITY}-1"
 
 
 async def test_async_added_to_hass(default_sensor):
@@ -90,12 +90,12 @@ async def test__temp2c():
 
 async def test_async_update(hass: HomeAssistant, default_sensor):
     """Test sensor update."""
-    hass.states._states[TEST_WEATHER_ENTITY] = None
+    hass.states._states[MOCK_WEATHER_ENTITY] = None
 
     with raises(HomeAssistantError):
         await default_sensor.async_update()
 
-    hass.states.async_set(TEST_WEATHER_ENTITY, "State")
+    hass.states.async_set(MOCK_WEATHER_ENTITY, "State")
 
     with raises(HomeAssistantError):
         await default_sensor.async_update()
@@ -118,12 +118,12 @@ async def test_async_update(hass: HomeAssistant, default_sensor):
             ATTR_FORECAST_TEMP: 8,
         },
         {
-            ATTR_FORECAST_TIME: today_ts + (TEST_DAYS + 1) * days,
+            ATTR_FORECAST_TIME: today_ts + (MOCK_DAYS + 1) * days,
         },
     ]
 
     hass.states.async_set(
-        TEST_WEATHER_ENTITY,
+        MOCK_WEATHER_ENTITY,
         "State",
         attributes={
             ATTR_WEATHER_TEMPERATURE: -1,
@@ -137,7 +137,7 @@ async def test_async_update(hass: HomeAssistant, default_sensor):
     assert default_sensor.icon == ICON_WINTER
 
     hass.states.async_set(
-        TEST_WEATHER_ENTITY,
+        MOCK_WEATHER_ENTITY,
         "State",
         attributes={
             ATTR_WEATHER_TEMPERATURE: 9.9,
@@ -151,7 +151,7 @@ async def test_async_update(hass: HomeAssistant, default_sensor):
     assert default_sensor.icon == ICON_WINTER
 
     hass.states.async_set(
-        TEST_WEATHER_ENTITY,
+        MOCK_WEATHER_ENTITY,
         "State",
         attributes={
             ATTR_WEATHER_TEMPERATURE: 10,
