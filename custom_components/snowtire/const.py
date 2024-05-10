666dc78922874dad69a9184980e88526fc37f099
Andrey Khrolenok <andrey@khrolenok.ru>
2024-05-10 13:22:32 +0300

    Bump version to 1.4.9

--- custom_components/snowtire/const.py
+++ custom_components/snowtire/const.py
@@ -9,7 +9,7 @@ from typing import Final
 # Base component constants
 NAME: Final = "Snowtire Sensor"
 DOMAIN: Final = "snowtire"
-VERSION: Final = "1.4.9-alpha"
+VERSION: Final = "1.4.9"
 ISSUE_URL: Final = "https://github.com/Limych/ha-snowtire/issues"
 
 STARTUP_MESSAGE: Final = f"""
